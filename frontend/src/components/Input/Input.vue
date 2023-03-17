b780b1dc3ba01da695e467d53567a8fedda7c634
Karlo <karlo.andre.lee@sun-asterisk.com>
2023-03-17 15:03:19 +0800

    Create Page

---
+++ frontend/src/components/Input/Input.vue
@@ -0,0 +1,19 @@
+<script setup>
+const props = defineProps({
+  label: String,
+  name: String,
+  modelValue: String,
+});
+const emit = defineEmits(["update:modelValue"]);
+</script>
+
+<template>
+  <p class="text-2xl mb-2">{{ label }}</p>
+  <input
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    :name="name"
+    type="text"
+    class="border-2 border-lime-900 mb-4 w-full h-8"
+  />
+</template>
--- frontend/src/views/notes/NoteCreate.vue
+++ frontend/src/views/notes/NoteCreate.vue
@@ -1,3 +1,28 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import Input from "../../components/Input/Input.vue";
 
-<template>create notes</template>
+const title = ref("");
+const content = ref("");
+const label = ref("");
+</script>
+
+<template>
+  <div class="w-fit">
+    <Input label="Note Title" name="title" v-model:model-value="title" />
+
+    <p class="text-2xl">Note Content</p>
+    <textarea
+      v-model="content"
+      rows="10"
+      cols="50"
+      name="content"
+      class="border-2 border-lime-900 mb-2"
+    />
+
+    <Input label="Note Label" name="label" v-model:model-value="label" />
+    <button class="w-full p-2 rounded border bg-cyan-400 text-xl">
+      Submit
+    </button>
+  </div>
+</template>
