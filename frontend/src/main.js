b556378a518e94862ad4dc1a42dc6a860abd3e7d
Karlo <karloandre.lee@sun-asterisk.com>
2023-03-18 15:34:52 +0800

    Use headless/ui

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,22 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-import './assets/main.css'
+import "./assets/main.css";
 
-const app = createApp(App)
+// Vuetify
+// import "vuetify/styles";
+// import { createVuetify } from "vuetify";
+// import * as components from "vuetify/components";
+// import * as directives from "vuetify/directives";
 
-app.use(router)
+// const vuetify = createVuetify({
+//   components,
+//   directives,
+// });
 
-app.mount('#app')
+const app = createApp(App);
+
+app.use(router);
+
+app.mount("#app");
--- frontend/src/views/notes/NoteCreate.vue
+++ frontend/src/views/notes/NoteCreate.vue
@@ -1,5 +1,11 @@
 <script setup>
-import { reactive, onMounted } from "vue";
+import { reactive, onMounted, ref, computed } from "vue";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxOptions,
+  ComboboxOption,
+} from "@headlessui/vue";
 
 import useNotes from "../../api/notes";
 import useLabels from "../../api/labels";
@@ -13,6 +19,24 @@ const form = reactive({
   label: "",
 });
 onMounted(() => getLabels());
+
+const people = [
+  "Durward Reynolds",
+  "Kenton Towne",
+  "Therese Wunsch",
+  "Benedict Kessler",
+  "Katelyn Rohan",
+];
+const selectedPerson = ref([labels.value[0], labels.value[1]]);
+const query = ref("");
+
+const filteredLabels = computed(() =>
+  query.value === ""
+    ? labels
+    : labels.value.filter((label) => {
+        return label.name.toLowerCase().includes(query.value.toLowerCase());
+      })
+);
 </script>
 
 <template>
@@ -43,6 +67,26 @@ onMounted(() => getLabels());
           type="textfield"
           v-model:model-value="form.label"
         />
+        <Combobox v-model="selectedPerson" multiple>
+          <ul v-if="selectedPerson.length > 0">
+            <li v-for="person in selectedPerson" :key="person.id">
+              {{ person.name }}
+            </li>
+          </ul>
+          <ComboboxInput
+            @change="query = $event.target.value"
+            :displayValue="(label) => label.name"
+          />
+          <ComboboxOptions>
+            <ComboboxOption
+              v-for="label in filteredLabels"
+              :key="label.id"
+              :value="label"
+            >
+              {{ label.name }}
+            </ComboboxOption>
+          </ComboboxOptions>
+        </Combobox>
         <button class="p-2 rounded border bg-yellow-300 text-xl" type="submit">
           Submit
         </button>
